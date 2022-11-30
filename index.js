e11143c94c758321a1a9da8b078912f824b42c88
sirMelka <melkabotale1@gmail.com>
2022-11-30 23:53:22 +0100

    feat:finalisation of screens 2 and 3

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-let btns = document.querySelectorAll(".section .btn ")
+let btns = document.querySelectorAll(".section .next ")
+let confirmationBtn = document.querySelector(".accept")
+let popupBtns = document.querySelectorAll(".popup-btn")
+let confirmForm = document.querySelector(".confirm-form")
 
 
-btns.forEach((btn) => {
+console.log(popupBtns);
+btns.forEach((btn,index) => {
     btn.addEventListener('click',(event)=>{
         event.preventDefault()
         const section = btn.parentNode.parentNode
-        section.style.display="none"
+        index<btns.length?section.style.display="none":""
         section.nextElementSibling.style.display="flex"
-
+        // alert(index)
     })
 })
 
+confirmationBtn.addEventListener('click', (event)=>{
+    event.preventDefault()
+    let popup = confirmationBtn.parentNode.parentNode.parentNode.nextElementSibling
+    popup.style.display="flex"
+})
+popupBtns.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        btn.parentNode.parentNode.parentElement.style.display="none"
+        popupBtns[1].addEventListener('click',()=>{
+            confirmForm.submit()
+        })
+    })
+})
 console.log(btns);
